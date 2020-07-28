409cdb8873f64d7cdc401a57eee4364439dbbc4a
Lautaro Escudero <lauta66@gmail.com>
2020-07-28 12:03:10 -0300

    Subiendo back a github

---
+++ src/main/java/com/example/controller/InsumoController.java
@@ -0,0 +1,66 @@
+package com.example.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entitie.ArticuloInsumo;
+import com.example.service.InsumoService;
+
+@RestController
+@RequestMapping("/insumo")
+public class InsumoController {
+
+	@Autowired
+	@Qualifier("insumoservicio")
+	private InsumoService insumoService;
+
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping("/agregar")
+	public boolean agregar(@RequestBody @Valid ArticuloInsumo insumo) {
+		return insumoService.crear(insumo);
+	}
+
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/insumos")
+	public List<ArticuloInsumo> getInsumos() {
+
+		return insumoService.getInsumos();
+
+	}
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/sinstock")
+	public List<ArticuloInsumo> getInsumoSinStock() {
+
+		return insumoService.insumoSinStock();
+
+	}
+	
+	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/pages")
+    public ResponseEntity<Page<ArticuloInsumo>> paginas(
+    		 @RequestParam(defaultValue = "0") int page,
+             @RequestParam(defaultValue = "4") int size
+             ){
+        Page<ArticuloInsumo> insumos = insumoService.paginas(PageRequest.of(page, size));
+       
+        return new ResponseEntity<Page<ArticuloInsumo>>(insumos, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/example/repository/ManufacturadoRepository.java
@@ -0,0 +1,26 @@
+package com.example.repository;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.entitie.ArticuloManufacturado;
+import com.example.entitie.RubroGeneral;
+
+@Repository("manufacturadorepositorio")
+public interface ManufacturadoRepository extends JpaRepository<ArticuloManufacturado, Serializable>,
+		PagingAndSortingRepository<ArticuloManufacturado, Serializable> {
+	public abstract Page<ArticuloManufacturado> findAll(Pageable pageable);
+
+	public abstract Page<ArticuloManufacturado> findByFechaBaja(Date fecha, Pageable pageable);
+	
+	public abstract Page<ArticuloManufacturado> findByFechaBajaAndRubro(Date fecha,RubroGeneral rubro, Pageable pageable);
+	
+	public abstract ArticuloManufacturado findByDenominacion(String denominacion);
+
+}
